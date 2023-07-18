7e26127df3194d42255b6e03f9c6b9a779f391bc
nataliaL <sta491@yandex.ru>
2023-07-18 17:35:30 +0300

    add calculateSum

---
+++ Task36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int CalculatePositionSum(int[] array)
+    {
+        int sum = 0;
+        
+        for (int i = 1; i < array.Length; i += 2) // Обход элементов на нечетных позициях
+        {
+            sum += array[i];
+        }
+        
+        return sum;
+}
+
+int[] GenerateRandomArray(int length)
+{
+    int[] array = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(-100, 100); // Генерация случайного трехзначного числа
+    }
+    return array;
+}
+
+int[] numbers = GenerateRandomArray(4); // Замените 4 на желаемый размер массива
+Console.WriteLine($"[{String.Join(",", numbers)}]");
+
+int result = CalculatePositionSum(numbers);
+Console.WriteLine("Сумма элементов на нечетных позициях: " + result);
