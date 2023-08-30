a80850139082cfb3e0ad49a1c24bef3f69a38102
nataliaL <sta491@yandex.ru>
2023-08-30 17:14:48 +0300

    create array

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,38 +6,55 @@
 // [“Russia”, “Denmark”, “Kazan”] → []
 
 string[] GetThreeSymbol(string[] inputArray)
+{
+    if (inputArray.Length == 0)
     {
-        int count = 0;
-        
-        // Считаем, сколько строк удовлетворяют условию
-        for (int i = 0; i < inputArray.Length; i++)
-        {
-            if (inputArray[i].Length <= 3)
-            {
-                count++;
-            }
-        }
-        
-        string[] resultArray = new string[count]; // Создаем новый массив на основе подсчитанного количества строк
-        
-        int index = 0;
-        for (int i = 0; i < inputArray.Length; i++)
-        {
-            if (inputArray[i].Length <= 3)
-            {
-                resultArray[index] = inputArray[i];
-                index++;
-            }
-        }
-
-        return resultArray;
+        return new string[0];
     }
 
-Console.Write("Введите количество строк в массиве: ");
-int n = Convert.ToInt32(Console.ReadLine());
-string[] array = new string[n];
+    int count = 0;
 
+    // Считаем, сколько строк удовлетворяют условию
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
 
-string[] newArray = GetThreeSymbol(array);
-        Console.WriteLine($"[{string.Join(",", newArray)}]");
+    string[] resultArray = new string[count]; // Создаем новый массив на основе подсчитанного количества строк
 
+    int index = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            resultArray[index] = inputArray[i];
+            index++;
+        }
+    }
+
+    return resultArray;
+}
+string[] CreateArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите элементы массива");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+int GetInput(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int size = GetInput("Введите количество строк в массиве: ");
+string[] array = CreateArray(size);
+
+string[] newArray = GetThreeSymbol(inputArray);
+Console.WriteLine($"[{string.Join(",", newArray)}]");
